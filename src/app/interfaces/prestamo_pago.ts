83699130732fe3e1357b5a54b65b689013d554ac
Elmer Ramirez <elmer.yam@puntosingular.com>
2025-04-19 00:47:25 -0500

    agregar interfaces para la nomina

---
+++ src/app/interfaces/prestamo_pago.ts
@@ -0,0 +1,10 @@
+export interface prestamo_pago {
+    uniqueId?: number;
+    Bit_Activo?: number;
+    nomina?: number;
+    saldo: number;
+    fecha: Date;
+    numero_pago: string;
+    status: string;
+    prestamo: number;
+}
