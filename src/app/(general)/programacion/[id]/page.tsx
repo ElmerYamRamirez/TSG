579c3d91741042b2cd81a7fe20cd293c568cef5e
Elmer Ramirez <elmer.yam@puntosingular.com>
2025-05-23 17:19:13 -0500

    Mejoras Visuales para /programacion/[id]

--- src/app/(general)/programacion/[id]/page.tsx
+++ src/app/(general)/programacion/[id]/page.tsx
@@ -14,10 +14,21 @@ export default async function ProgramacionPage({params} : Props){
 
   return (
     <div className="max-w-6xl mx-auto px-4 py-8">
-      <h1>Programacion {id}</h1>
-      <Link href={"/programaciones"} className="bg-indigo-600 hover:bg-indigo-700 text-white px-4 py-2 rounded-md text-sm font-medium">
-        Regresar
-      </Link>
+      <h1 className="text-3xl font-bold text-center text-indigo-700">Detalle Programacion</h1>
+      <div className="flex justify-between mb-4">
+        <div>
+          <p><strong>Folio:</strong> {programacion?.programacion.folio}</p>
+          <p><strong>Fecha programada:</strong> {(programacion?.programacion.Fecha_programada).toLocaleString("es-MX", {day: "2-digit",month:"2-digit",year: "2-digit",})}</p>
+          <p><strong>Destino:</strong> {programacion?.programacion.nombre_destino}</p>
+          <p><strong>Cliente:</strong> {programacion?.programacion.cliente_name}</p>
+          <p><strong>Operador:</strong> {programacion?.programacion.operador_name}</p>
+        </div>
+        <div>
+          <Link href={"/programaciones"} className="bg-indigo-600 hover:bg-indigo-700 text-white px-4 py-2 rounded-md text-sm font-medium">
+            Regresar
+          </Link>
+        </div>
+      </div>
 
       <Tabs programacion={programacion?.programacion}></Tabs>
     </div>
--- src/components/programacion/viaticos.tsx
+++ src/components/programacion/viaticos.tsx
@@ -151,7 +151,7 @@ export default function Viaticos({ viaticos, programacion }: { viaticos: Viatico
       {isModalOpen && itemEditando && (
         <div className="fixed inset-0 bg-black/50 bg-opacity-50 flex items-center justify-center z-50">
           <div className="bg-white p-6 rounded shadow w-full max-w-md space-y-4">
-            <h2 className="text-lg font-bold mb-2">Editar Viatico</h2>
+            <h2 className="text-lg font-bold mb-2">{isEditing ? 'Editar Viatico' : 'Agregar Viatico'}</h2>
             <input
               type="text"
               className="border rounded px-3 py-1 w-full"
@@ -172,7 +172,7 @@ export default function Viaticos({ viaticos, programacion }: { viaticos: Viatico
               </button>
               <button className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
                 onClick={guardarCambios}>
-                Guardar
+                {isEditing ? 'Guardar' : 'Crear'}
               </button>
             </div>
 
